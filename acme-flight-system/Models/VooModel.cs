8ec9879b30507a656e2258b6542aec5a87791464
ecoltri2 <edr.ccol@gmail.com>
2020-12-06 20:46:28 -0300

    Flight model added

---
+++ acme-flight-system/Models/VooModel.cs
@@ -0,0 +1,11 @@
+﻿namespace acme_flight_system.Models
+{
+    class VooModel
+    {
+        public int ID_VOO { get; set; }
+        public System.DateTime Data { get; set; }
+        public double Custo { get; set; }
+        public int Distancia { get; set; }
+        public int Nivel_dor { get; set; }
+    }
+}
